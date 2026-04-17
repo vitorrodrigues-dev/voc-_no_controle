6521c91ae0d3a0e043d24de367f423b77a3d8e0c
Vitor Rodrigues <rodriguesvitorcubo14@gmail.com>
2026-04-17 09:56:59 -0300

    Transformando em web App

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(_name_)
 
-@app.route("/")
-def home():
-    return "Sistema você no controle rodando"
+@app.route('/')
+def menu_principal():
+    return """
+    <h1>Menu Principal</h1>
+    <p>Escolha uma opção:</p>
+    <ul>
+        <li><a href="/dieta">1. Ver Dieta</a></li>
+        <li><a href="/ficha">2. Ver Ficha</a></li>
+    </ul>
+    """
+
+@app.route('/dieta')
+def dieta():
+    # Aqui você chama a lógica do seu arquivo dieta.py
+    return "Aqui aparece a dieta (Puxado do seu código antigo)"
+
+@app.route('/ficha')
+def ficha():
+    # Aqui você chama a lógica do seu arquivo ficha.py
+    return "Aqui aparece a ficha"
+
+if _name_ == "_main_":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
